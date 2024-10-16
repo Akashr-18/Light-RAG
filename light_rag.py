a3a366de9aea20d3a57f49a16a4de385f12be4bf
Akash R <115867523+Akashr-18@users.noreply.github.com>
2024-10-16 10:34:51 +0530

    Update light_rag.py

--- light_rag.py
+++ light_rag.py
@@ -7,3 +7,11 @@ WORKING_DIR = "./dickens"
 if not os.path.exists(WORKING_DIR):
     os.mkdir(WORKING_DIR)
 
+rag = LightRAG(
+    working_dir=WORKING_DIR,
+    llm_model_func=gpt_4o_mini_complete  # Use gpt_4o_mini_complete LLM model
+    # llm_model_func=gpt_4o_complete  # Optionally, use a stronger model
+)
+
+
+
